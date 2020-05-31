61878866b2e3858d9281652e2e0ea670bfda0f94
Vaviloff <vlad@vaviloff.ru>
2020-05-31 19:16:14 +0700

    doesn't work on 2 diff. machines w. Mint 19.2

---
+++ index.js
@@ -0,0 +1,20 @@
+// it augments the installed puppeteer with plugin functionality
+const puppeteer = require('puppeteer-extra')
+ 
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+ 
+// puppeteer usage as normal
+puppeteer.launch({ 
+  // executablePath: '/usr/bin/google-chrome',
+  headless: true
+  }).then(async browser => {
+  console.log('Running tests..')
+  const page = await browser.newPage()
+  await page.goto('https://bot.sannysoft.com')
+  await page.waitFor(5000)
+  await page.screenshot({ path: 'testresult.png', fullPage: true })
+  await browser.close()
+  console.log(`All done, check the screenshot. ✨`)
+});
